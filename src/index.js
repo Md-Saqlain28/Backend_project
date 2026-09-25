d60ff7587d0f89bba075c96706c75edd2f123454
Mohammed Saqlain <mdsaqlain2486@gmail.com>
2026-09-25 22:27:26 +0530

    Data base name and connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello from the Backend Project")
+import dotenv from "dotenv"
+import { app } from "./app.js";
+import { connect } from "mongoose";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: "./.env"
+})
+
+
+const PORT = process.env.PORT || 8001
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("Mongodb connection err", err);
+})
+
