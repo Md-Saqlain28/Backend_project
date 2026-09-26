178a7fed16baca0c37db4467d0449fa548e6db75
Mohammed Saqlain <mdsaqlain2486@gmail.com>
2026-09-26 21:54:09 +0530

    Health check controller - to check if the system is responding

---
+++ src/controllers/healthcheck_controllers.js
@@ -0,0 +1,12 @@
+import { ApiResponse} from "../utils/Apiresponse.js"
+import {asyncHandler} from "../utils/AsyncHandler.js"
+
+
+
+const healthcheck = asyncHandler( async (req, res) => {
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "OK", "Health Check Passed"))
+})
+
+export {healthcheck}
